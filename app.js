0cf91e0fa4338c126785bfed4a5db75337164eed
AnnaNThomas <annathomas2003ned@gmail.com>
2024-06-13 15:47:11 +0530

    until token created

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
-const bcrypt=require("bcryptjs")
+const bcrypt = require("bcryptjs")
 const { usermodel } = require("./models/blog")
+const jwt = require("jsonwebtoken")
 
 const app = express()
 app.use(cors())
@@ -12,27 +13,69 @@ mongoose.connect("mongodb+srv://annant2003:annant2003anna@cluster0.ncbl8ds.mongo
 
 //generate generateHashedpassword arrow function
 //making this function to asynchronous function
-const generateHashedpassword= async(password)=>{
-    const salt=await bcrypt.genSalt(10)            //salt value is cost factor  value
-    return bcrypt.hash(password,salt)             //encrypt function
+const generateHashedpassword = async (password) => {
+    const salt = await bcrypt.genSalt(10)            //salt value is cost factor  value
+    return bcrypt.hash(password, salt)             //encrypt function
 
 }
 
-app.post("/signUp",async(req,res)=>{
+app.post("/signUp", async (req, res) => {
 
-    let input =req.body
-    let hashedpassword= await generateHashedpassword(input.password)  
+    let input = req.body
+    let hashedpassword = await generateHashedpassword(input.password)
     console.log(hashedpassword)
-    input.password=hashedpassword 
+    input.password = hashedpassword
 
     let blog = new usermodel(input)
     blog.save()
     res.json({ "status": "success" })
 })
 
+
+//api for signIn
+
+app.post("/signIn", (req, res) => {
+    let input = req.body
+    usermodel.find({ "emailid": req.body.emailid }).then(
+        (response) => {
+            if (response.length > 0) {
+                let dbpassword = response[0].password
+                console.log(dbpassword)
+
+                bcrypt.compare(input.password, dbpassword, (error, isMatch) => {             //password comparing
+                    if (isMatch) {
+                        //if login success generate token
+                        jwt.sign({ email: input.emailid }, "blog-app", { expiresIn: "1d" },       //blog-app is token name
+                            (error, token) => {                         
+                                if (error) {
+                                    res.json({ "status": "unable to create token" })
+                                } else {
+                                    res.json({ "status": "success", "userid": response[0]._id, "token": token })
+                                }
+                            }
+                        )
+
+                    } else {
+                        res.json({ "status": "Incorrect" })
+                    }
+
+                })
+
+
+
+
+            } else {
+                res.json({ "status": "user not found" })
+            }
+
+
+        }
+    ).catch()
+})
+
 // let input = req.body
 //let employee = new employeemodel(input)
 
-app.listen(8080,()=>{
+app.listen(8080, () => {
     console.log("server started")
 })
